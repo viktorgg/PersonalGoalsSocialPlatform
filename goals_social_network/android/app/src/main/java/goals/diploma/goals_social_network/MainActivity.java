488105b804dbfb6d409d39179e24c96cc440ba1f
viktorgg <viktor.grgv47@gmail.com>
2024-06-13 14:15:14 +0300

    Remove unnecessary folder

--- goals_social_network/android/app/src/main/java/goals/diploma/goals_social_network/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package goals.diploma.goals_social_network;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- goals_social_network/lib/services/validators.dart
+++
@@ -1,9 +0,0 @@
-String? validateEmail(String value) {
-  RegExp regex = RegExp(
-      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$');
-   if (!regex.hasMatch(value)) {
-    return "Please provide a valid email address";
-  } else {
-     return null;
-   }
-}
